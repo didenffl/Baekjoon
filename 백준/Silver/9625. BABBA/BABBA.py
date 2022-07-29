789e085bc5d6daa8b7d4db31907393520f074eae
didenffl <109195918+didenffl@users.noreply.github.com>
2022-07-29 16:09:05 +0900

    Update BABBA.py

--- 백준/Silver/9625. BABBA/BABBA.py
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -8,3 +8,32 @@ for i in range(n):
     A.append(A[-1]+A[-2])
     B.append(B[-1]+B[-2])
 print(A[n], B[n])
+
+
+
+# 시간초과
+
+# from collections import deque 
+# import sys 
+# input = sys.stdin.readline 
+
+# n = int(input())
+# que = deque(['A'])
+# a = 0 
+# b = 0
+# for _ in range(n):
+#     l = len(que)
+#     for i in range(l):
+#         s = que.popleft()
+#         if s == 'A':
+#             que.append('B')
+#         else:
+#             que.append('B')
+#             que.append('A')
+# for _ in range(len(que)):
+#     l = que.pop()
+#     if l == 'A':
+#         a += 1 
+#     else:
+#         b += 1
+# print(a, b)
