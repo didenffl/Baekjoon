4a1202d2aa01be31a71bb301810dc8a3d09662ed
didenffl <109195918+didenffl@users.noreply.github.com>
2023-06-01 16:29:05 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT date_format(datetime, "%H"), count(*) as count
+SELECT date_format(datetime, "%H") hour, count(*) as count
 from animal_outs
 where date_format(datetime, "%H") between 9 and 20
 group by date_format(datetime, "%H")
